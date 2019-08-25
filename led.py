c457cad450e7ec849821c7b93bbd37ccbaff9a13
Steve Murch <steve@murch.net>
2019-08-25 13:39:38 -0700

    Adding sounds and led sample

---
+++ led.py
@@ -0,0 +1,13 @@
+#led.py
+# pin 13 (board number 13)
+
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(13,GPIO.OUT)
+print("LED on")
+GPIO.output(13,GPIO.HIGH)
+time.sleep(1)
+print("LED off")
+GPIO.output(13,GPIO.LOW)
