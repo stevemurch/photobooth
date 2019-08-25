bb585e48367673e143887ce3eb6893a89d49da28
Steve Murch <steve@murch.net>
2019-08-25 13:38:42 -0700

    Sound effects

--- photobooth.py
+++ photobooth.py
@@ -1,4 +1,5 @@
 # PhotoBooth
+# LED on BOARD PIN 13
 # 
 # wait for button press
 #     count down 3..2..1
@@ -16,22 +17,28 @@
 # arcade wait mode
 #     
 
-from Tkinter import *
+from tkinter import *
 from time import sleep
 import subprocess
 from PIL import Image, ImageTk
+import RPi.GPIO as GPIO
+
+LED_BOARD_PIN = 13
+
+
+
 
 def showButton():
-	btn.lift()
+    btn.lift()
 
 def hideButton():
-	btn.lower()
+    btn.lower()
 
 def updatePhoto():
     global img
     n=1
     same = True 
-	
+    
     path = "image2.jpg"
     image = Image.open(path)
     [imageSizeWidth, imageSizeHeight] = image.size
@@ -43,9 +50,9 @@ def updatePhoto():
 
     image = image.resize((newImageSizeWidth, newImageSizeHeight), Image.ANTIALIAS)
     img = ImageTk.PhotoImage(image)
-	
-	
-	
+    
+    
+    
     #print("updating image...")
     #global img
     #img = ImageTk.PhotoImage(Image.open("image2.jpg"))
@@ -55,9 +62,28 @@ def updatePhoto():
     canvas.grid(column=0,row=1)
     canvas.update()
 
+def flashLightOn():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)
+    GPIO.setup(LED_BOARD_PIN,GPIO.OUT)
+    print("LED on")
+    GPIO.output(LED_BOARD_PIN,GPIO.HIGH)
+
+
+
+def flashLightOff():
+    print("LED off")
+    GPIO.output(LED_BOARD_PIN,GPIO.LOW)
+    
+
 def takePhoto():
-	subprocess.Popen(["fswebcam", "-r","800x600", "image2.jpg"])
-	lbl.configure(text="PHOTO SNAPPED!")
+    subprocess.Popen(["fswebcam", "-r","800x600","--no-banner", "image2.jpg"])
+    lbl.configure(text="PHOTO SNAPPED!")
+
+def playCameraSound():
+    # requires mpg321 install first
+    subprocess.Popen(["mpg321","camera.mp3"])
+    
 
 def countdown():
     hideButton()
@@ -69,32 +95,45 @@ def countdown():
     sleep(1)
     lbl.configure(text="1...")
     lbl.update()
-    sleep(1)
+    sleep(0.8)
+    playCameraSound()
+    lbl.configure(text="SNAP!")
+    lbl.update()
+    flashLightOn()
     takePhoto()
-    sleep(3)
+    sleep(0.25)
+    flashLightOff()
+    lbl.update()
+    
+    sleep(2)
     updatePhoto()
+    lbl.configure(text="Press the button to take a photo!")
+    lbl.update()
+    
 
 def clicked():
-	btn.grid_remove()
-	countdown()
-	add_button()
+    btn.grid_remove()
+    countdown()
+    add_button()
 
 def add_button():
-	btn.grid(column=1, row=0)
+    btn.grid(column=0, row=2)
 
 root = Tk()
-root.geometry('800x600')
+root.geometry('800x800')
 root.title("Photo Booth")
 
 lbl = Label(root, text="Press the button to take a photo!", font=("Arial Bold", 20))
 lbl.grid(column=0, row=0)
 
-btn = Button(root, text="Take Photo", command=clicked)
-btn.grid(column=1, row=0)
+
 
 canvas = Canvas(root, width = 800, height = 600) 
 img = ImageTk.PhotoImage(Image.open("image.jpg"))      
 canvas.create_image(0,0, anchor=CENTER, image=img) 
 canvas.grid(column=0,row=1)
+
+btn = Button(root, text="Take Photo", command=clicked)
+btn.grid(column=0, row=2)
 
 root.mainloop()
